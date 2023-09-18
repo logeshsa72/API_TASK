f570050ed029fa14e3d038766c420cef59c862ea
logeshsa72 <logeshsmart72@gmail.com>
2023-09-18 19:58:11 +0530

    extra add

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ getthiData = (number)=>{
 
     const apiUrl = "https://api-thirukkural.vercel.app/api";
 
-    const Full_Url = `${Url}?num=${number}`;
+    const Full_Url = `${apiUrl}?num=${number}`;
 
     const thiPromise = fetch(Full_Url);
 
@@ -21,6 +21,12 @@ function searchNumber(){
 
     .then((response) =>{
         console.log(response)
+
+        document.getElementById('kural').innerText = response.line1;
+        document.getElementById('kural1').innerText = response.line2;
+        document.getElementById('exp').innerText = response.tam_exp
+;
+
     })
     .catch((err)=>{
         console.log(err)
